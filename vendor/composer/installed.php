1476318f84e33ac0ada00bbd6ffce3d3e8278b71
HuiZhang <mr.bestjane@gmail.com>
2021-10-20 15:30:16 +0800

    add test name

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'bb719b3579f60f6b94e2deebc4389a13174badfc',
-        'name' => 'ph/t3',
+        'reference' => 'de7b33f154ea807c229190ca36d54c0e5e6267f9',
+        'name' => 'ph1w1asd23/t3',
         'dev' => true,
     ),
     'versions' => array(
-        'ph/t3' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
+        'ph1w1asd23/t3' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'bb719b3579f60f6b94e2deebc4389a13174badfc',
+            'reference' => 'de7b33f154ea807c229190ca36d54c0e5e6267f9',
             'dev_requirement' => false,
         ),
     ),
