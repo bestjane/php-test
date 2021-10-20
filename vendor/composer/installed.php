de7b33f154ea807c229190ca36d54c0e5e6267f9
HuiZhang <mr.bestjane@gmail.com>
2021-10-20 15:25:42 +0800

    init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'bb719b3579f60f6b94e2deebc4389a13174badfc',
+        'name' => 'ph/t3',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ph/t3' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'bb719b3579f60f6b94e2deebc4389a13174badfc',
+            'dev_requirement' => false,
+        ),
+    ),
+);
